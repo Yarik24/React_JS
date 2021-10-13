e68463db92466a976e3b2047c0fae48ae8680f21
Yarik24 <jarslavkblv24@gmail.com>
2021-10-13 16:00:06 +0300

    hw_4

---
+++ first-app/src/Components/App.js
@@ -0,0 +1,32 @@
+import Message from "./Message/Message";
+import { useState, useEffect } from "react";
+import InputForm from "./InputForm/InputForm";
+import Chats from "./Chats/Chats";
+
+
+function App() {
+  const messageText = "Hello, world! It's my first props!";
+  const [messageList, setMessageList] = useState([{ author: 'Yarik', text: 'Welcom!' }, { author: 'Yarik', text: 'Bay!' }]);
+
+   useEffect(() => {
+    if (messageList[messageList.length - 1].author !== 'Yarik') {
+      setTimeout(() => {
+        setMessageList((prev) => [...prev, { author: 'Yarik', text: 'What is hapenning?' }])
+      }, 3000);
+    }
+   }, [messageList]);
+  
+  return (
+    <div className="App">
+      <Chats />
+      
+      <>
+        <Message messageList={messageList} />
+        <InputForm setMessageList={setMessageList} />
+      </>
+     
+    </div>
+  );
+}
+
+export default App;
